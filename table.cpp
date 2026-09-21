c16f1152a579f29fb6834be764df565b86b5f5ae
peterouob <hellolinpeter@gmail.com>
2026-09-21 15:45:36 +0800

    feat: hash table and using fibonacci hash be the main hash algo

--- table.cpp
+++ table.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "table.h"
+
+#include <utility>
+
+constexpr std::size_t Table::hash(const void *ptr) noexcept {
+    const auto ad = reinterpret_cast<uintptr_t>(ptr);
+    return ad * 11400714819323198485ULL;
+}
+
+std::size_t Table::probe(const std::size_t i, const std::size_t j) const noexcept {
+    return (i - j + 1) & (this->items_.size() - 1);
+}
+
+std::size_t Table::ideal_size(const std::size_t n) const noexcept {
+    auto target = static_cast<std::size_t>(static_cast<double>(n + 1) / this->load_factor_);
+    target = std::max<std::size_t>(target, 8);
+
+    return std::bit_ceil(target);
+}
+
+void Table::rehash(const std::size_t new_nslots) {
+    for (const std::vector<Entry> old = std::exchange(items_, std::vector<Entry>(new_nslots)); const Entry& e: old) {
+        if (!e.empty()) {
+            insert_no_grow(e);
+        }
+    }
+}
+
+void Table::insert_no_grow(const Entry &e) noexcept{
+    std::size_t i = ideal_slot(e.hash);
+    std::size_t dist = 0;
+    while (true) {
+        Entry &cur = items_[i];
+        if (cur.empty()) {
+            cur = e;
+            return;
+        }
+
+        if (probe(i, cur.hash) < dist) {
+            dist = probe(i, cur.hash);
+            std::swap(this->items_[i], cur);
+        }
+
+        i = (i + 1) & (this -> items_.size() - 1);
+        dist += 1;
+    }
+}
+
+std::size_t Table::ideal_slot(const std::size_t n) const noexcept {
+    constexpr int bits = std::numeric_limits<std::size_t>::digits;
+    return n >> (bits - std::countr_zero(this->items_.size()));
+}
--- table.h
+++ table.h
@@ -9,39 +9,43 @@
 #ifndef CPPTGC_TABLE_H
 #define CPPTGC_TABLE_H
 
-namespace tgcpp {
-    struct Entry {
-        void* ptr = nullptr;
-        std::size_t size = 0;
-        std::size_t hash = 0;
+struct Entry {
+    void* ptr = nullptr;
+    std::size_t size = 0;
+    std::size_t hash = 0;
+
+    [[nodiscard]] bool empty() const noexcept {
+        return ptr == nullptr ;
     };
+};
 
-    class Table {
-    public:
-        Table() = default;
-        void add(void *ptr, std::size_t size);
+class Table {
+public:
+    Table() = default;
+    void add(void *ptr, std::size_t size);
 
-        template<typename Self>
-        [[nodiscard]] auto find(this Self&& self, const void* ptr) noexcept;
+    template<typename Self>
+    [[nodiscard]] auto find(this Self&& self, const void* ptr) noexcept;
 
-        void remove(const void* ptrr) noexcept;
+    void remove(const void* ptr) noexcept;
 
-    protected:
-        [[nodiscard]] static constexpr std::size_t hash(const void* ptr) noexcept;
-        [[nodiscard]] std::size_t probe(std::size_t i, std::size_t j) const noexcept;
-        [[nodiscard]] std::size_t ideal_size(std::size_t n) const noexcept;
+protected:
+    [[nodiscard]] static constexpr std::size_t hash(const void* ptr) noexcept;
+    [[nodiscard]] std::size_t ideal_slot(std::size_t n) const noexcept;
+    [[nodiscard]] std::size_t probe(std::size_t i, std::size_t j) const noexcept;
+    [[nodiscard]] std::size_t ideal_size(std::size_t n) const noexcept;
 
-    private:
-        void grow();
-        void shrink();
-        void rehash(std::size_t new_nslots);
+private:
+    void grow();
+    void shrink();
+    void rehash(std::size_t new_nslots);
+    void insert_no_grow(const Entry &e) noexcept;
 
-        std::vector<Entry> items_;
-        std::size_t nitems_ = 0;
-        double load_factor_ = 0.9;
-        std::uintptr_t min_ptr_ = UINTPTR_MAX;
-        std::uintptr_t max_ptr_ = 0;
-    };
-}
+    std::vector<Entry> items_;
+    std::size_t nitems_ = 0;
+    double load_factor_ = 0.9;
+    std::uintptr_t min_ptr_ = UINTPTR_MAX;
+    std::uintptr_t max_ptr_ = 0;
+};
 
 #endif //CPPTGC_TABLE_H
