55d5d09c14ba4295cab26f933f3d2351684ac64b
peterouob <hellolinpeter@gmail.com>
2026-09-22 22:54:51 +0800

    feat: extend garbage collector with new APIs and PauseGuard class

--- cpptgc.h
+++ cpptgc.h
@@ -40,6 +40,18 @@ class Collector {
 
   void mark();
 
+  void collect();
+  void pause();
+  void resume();
+
+  [[nodiscard]] bool is_pause() const noexcept;
+
+  [[nodiscard]] std::expected<void*, std::errc> calloc(
+      std::size_t n, std::size_t size, Flags flags = Flags::None,
+      Destructor dtor = nullptr);
+  [[nodiscard]] std::expected<void*, std::errc> realloc(void* ptr,
+                                                        std::size_t size);
+
   [[nodiscard]] std::optional<Flags> flags_of(const void* ptr) const noexcept {
     const Entry* e = table_.find(ptr);
     if (e == nullptr) return std::nullopt;
@@ -54,6 +66,14 @@ class Collector {
 
   [[nodiscard]] std::size_t cur_size() const noexcept { return table_.size(); }
 
+  void set_flags(void* ptr, Flags flags) noexcept;
+  void set_dtor(void* ptr, Destructor dtor) noexcept;
+
+  [[nodiscard]] std::optional<std::size_t> size_of(
+      const void* ptr) const noexcept;
+  [[nodiscard]] std::optional<Destructor> dtor_of(
+      const void* ptr) const noexcept;
+
  protected:
   void clear_mark() noexcept;
 
@@ -65,10 +85,26 @@ class Collector {
 
   [[gnu::noinline]] void mark_stack();
 
+  [[nodiscard]] std::size_t collections() const noexcept;
+
+  void sweep();
+
  private:
   Table table_;
   const void* stack_bottom_;
   std::vector<std::span<const std::byte>> worklist_;
+};
+
+class [[nodiscard]] PauseGuard {
+ public:
+  explicit PauseGuard(Collector& gc) noexcept;
+  ~PauseGuard();
+  PauseGuard(const PauseGuard&) = delete;
+  PauseGuard& operator=(const PauseGuard&) = delete;
+
+ private:
+  Collector& gc_;
+  bool was_paused_;
 };
 
 #endif  // CPPTGC_CPPTGC_H
